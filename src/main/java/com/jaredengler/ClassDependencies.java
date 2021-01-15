6cfbc77bb7cc1c5a283ed2aa3020a86bbb1f33ea
Jared Engler <jared.lite@gmail.com>
2021-01-15 16:25:03 -0600

    define adjacency list spec

--- src/main/java/com/jaredengler/ClassDependencies.java
+++ src/main/java/com/jaredengler/ClassDependencies.java
@@ -1,7 +1,13 @@
 package com.jaredengler;
 
+import java.util.List;
+import java.util.Map;
+
 public class ClassDependencies {
   public static void main(String[] args) {
+  }
 
+  String calculatePath(Map<Integer, List<Integer>> classes) {
+    return "";
   }
 }
--- src/test/java/com/jaredengler/ClassDependenciesShould.java
+++ src/test/java/com/jaredengler/ClassDependenciesShould.java
@@ -2,13 +2,22 @@ package com.jaredengler;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 public class ClassDependenciesShould {
+
+  ClassDependencies classDependencies = new ClassDependencies();
+
   @Test
   void add_two_numbers() {
-    assertThat(1 + 1, is(equalTo(2)));
+    Map<Integer, List<Integer>> classes = Map.of(0, List.of());
+    assertThat(classDependencies.calculatePath(classes), isA(String.class));
   }
+
 }
