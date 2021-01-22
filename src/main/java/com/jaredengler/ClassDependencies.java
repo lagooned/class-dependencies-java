52ec1b31eddedcb2ba2d0f573bf4153e9f507e4f
Jared Engler <jared.lite@gmail.com>
2021-01-22 17:02:45 -0600

    implement dfs

--- src/main/java/com/jaredengler/ClassDependencies.java
+++ src/main/java/com/jaredengler/ClassDependencies.java
@@ -1,38 +1,60 @@
 package com.jaredengler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class ClassDependencies {
 
   String calculatePath(Map<Integer, List<Integer>> classes) {
-    return dfs(classes, 1).stream()
+    String order = dfs(classes).stream()
       .map(i -> i.toString())
       .reduce("", (a, b) -> b + " " + a)
       .trim();
+    System.out.println(order);
+    return order;
   }
 
-  public List<Integer> dfs(Map<Integer, List<Integer>> classes, Integer start) {
-    List<Integer> visited = new ArrayList<>();
-    System.out.println("classes " + classes);
-    dfs(classes, start, visited);
-    return visited;
-  }
-
-  private void dfs(Map<Integer, List<Integer>> classes, Integer current, List<Integer> visited) {
-    System.out.println("current " + current);
-    System.out.println("visited before adding " + visited);
-    visited.add(current);
-    System.out.println("visited after adding " + visited);
-    List<Integer> adj = classes.get(current);
-    System.out.println("adj " + adj);
-
-    for (Integer dest : adj) {
-      if (!visited.contains(dest)) {
-        dfs(classes, dest, visited);
+  public List<Integer> dfs(Map<Integer, List<Integer>> classes) {
+    // System.out.println("classes " + classes);
+    Map<Integer, Integer> parent = new HashMap<>();
+    List<Integer> order = new ArrayList<>();
+    for (Entry<Integer, List<Integer>> entry : classes.entrySet()) {
+      Integer current = entry.getKey();
+      if (!parent.containsKey(current)) {
+        parent.put(current, null);
+        dfsVisit(classes, current, parent, order);
       }
     }
+    // System.out.println("done " + parent);
+    return order;
+  }
+
+  private void dfsVisit(
+    Map<Integer, List<Integer>> classes,
+    Integer current,
+    Map<Integer, Integer> parent,
+    List<Integer> order
+  ) {
+
+    List<Integer> adj = classes.get(current);
+
+    // System.out.println("current => " + current);
+    // System.out.println("parent => " + parent);
+    // System.out.println("adj(" + current + ") => " + adj);
+
+    for (Integer destination : adj) {
+      if (!parent.containsKey(destination)) {
+        parent.put(destination, current);
+        dfsVisit(classes, destination, parent, order);
+      }
+    }
+
+    order.add(current);
+    // System.out.println("order " + order);
+
   }
 
 }
