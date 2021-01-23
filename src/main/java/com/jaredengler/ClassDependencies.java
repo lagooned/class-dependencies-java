7ed79f51c3cd20ae5353d5ef22e101932e67b5bd
Jared Engler <jared.lite@gmail.com>
2021-01-22 21:33:05 -0600

    clean up code and fix test cases

--- src/main/java/com/jaredengler/ClassDependencies.java
+++ src/main/java/com/jaredengler/ClassDependencies.java
@@ -1,26 +1,28 @@
 package com.jaredengler;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class ClassDependencies {
 
   String calculatePath(Map<Integer, List<Integer>> classes) {
     String order = dfs(classes).stream()
       .map(i -> i.toString())
-      .reduce("", (a, b) -> b + " " + a)
-      .trim();
+      .sorted(Comparator.reverseOrder())
+      .collect(Collectors.joining(" "));
     System.out.println(order);
     return order;
   }
 
   public List<Integer> dfs(Map<Integer, List<Integer>> classes) {
-    // System.out.println("classes " + classes);
     Map<Integer, Integer> parent = new HashMap<>();
     List<Integer> order = new ArrayList<>();
+    // graph isn't necessarily connected
     for (Entry<Integer, List<Integer>> entry : classes.entrySet()) {
       Integer current = entry.getKey();
       if (!parent.containsKey(current)) {
@@ -28,7 +30,6 @@ public class ClassDependencies {
         dfsVisit(classes, current, parent, order);
       }
     }
-    // System.out.println("done " + parent);
     return order;
   }
 
@@ -38,23 +39,15 @@ public class ClassDependencies {
     Map<Integer, Integer> parent,
     List<Integer> order
   ) {
-
     List<Integer> adj = classes.get(current);
-
-    // System.out.println("current => " + current);
-    // System.out.println("parent => " + parent);
-    // System.out.println("adj(" + current + ") => " + adj);
-
     for (Integer destination : adj) {
       if (!parent.containsKey(destination)) {
         parent.put(destination, current);
         dfsVisit(classes, destination, parent, order);
       }
     }
-
+    // record dfs post order
     order.add(current);
-    // System.out.println("order " + order);
-
   }
 
 }
--- src/test/java/com/jaredengler/ClassDependenciesShould.java
+++ src/test/java/com/jaredengler/ClassDependenciesShould.java
@@ -16,56 +16,33 @@ public class ClassDependenciesShould {
 
   @Test
   void returnAString() {
-
     Map<Integer, List<Integer>> classes = Map.of(0, List.of());
-    assertThat(
-      classDependencies.calculatePath(classes),
-      isA(String.class));
-
+    assertThat(classDependencies.calculatePath(classes), isA(String.class));
   }
 
   @Test
   void returnAStringOfCorrectSize() {
-
-    Map<Integer, List<Integer>> classes =
-      Map.of(0, List.of());
-
-    Map<Integer, List<Integer>> classes2 =
-      Map.of(0, List.of(),
-             1, List.of(0));
-
+    Map<Integer, List<Integer>> classes = Map.of(0, List.of());
+    Map<Integer, List<Integer>> classes2 = Map.of(0, List.of(), 1, List.of(0));
     assertThat(classDependencies.calculatePath(classes).length(), is(equalTo(1)));
-
     assertThat(classDependencies.calculatePath(classes2).length(), is(equalTo(3)));
-
   }
 
   @Test
   void handleOneDependency() {
-
-    Map<Integer, List<Integer>> classes =
-      Map.of(0, List.of(),
-             1, List.of(0));
-
-    assertThat(
-      classDependencies.calculatePath(classes),
-      is(equalTo("0 1")));
-
+    Map<Integer, List<Integer>> classes = Map.of(0, List.of(), 1, List.of(0));
+    assertThat(classDependencies.calculatePath(classes), is(equalTo("1 0")));
   }
 
   @Test
   void handleTwoDependency() {
-
-    Map<Integer, List<Integer>> classes =
-      Map.of(0, List.of(),
-             1, List.of(0),
-             2, List.of(0),
-             3, List.of(1, 2));
-
-    assertThat(
-      classDependencies.calculatePath(classes),
-      is(equalTo("3 1 2 0")));
-
+    Map<Integer, List<Integer>> classes = Map.of(
+      0, List.of(),
+      1, List.of(0),
+      2, List.of(0),
+      3, List.of(1, 2)
+    );
+    assertThat(classDependencies.calculatePath(classes), is(equalTo("3 2 1 0")));
   }
 
 }
