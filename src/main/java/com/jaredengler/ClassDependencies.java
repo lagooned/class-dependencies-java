07f398701ad45f1f372fdacb84e800c1b3162e11
Jared Engler <jared.lite@gmail.com>
2021-01-23 23:31:56 -0600

    impl dfs
     - clean up code and fix test cases
     - implement dfs
     - add dfs
     - fix compile

--- src/main/java/com/jaredengler/ClassDependencies.java
+++ src/main/java/com/jaredengler/ClassDependencies.java
@@ -1,16 +1,53 @@
 package com.jaredengler;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class ClassDependencies {
-  public static void main(String[] args) {
-  }
 
   String calculatePath(Map<Integer, List<Integer>> classes) {
-    return classes.keySet().stream()
+    String order = dfs(classes).stream()
       .map(i -> i.toString())
-      .reduce("", (a, b) -> b + " " + a).trim();
+      .sorted(Comparator.reverseOrder())
+      .collect(Collectors.joining(" "));
+    System.out.println(order);
+    return order;
   }
+
+  public List<Integer> dfs(Map<Integer, List<Integer>> classes) {
+    Map<Integer, Integer> parent = new HashMap<>();
+    List<Integer> order = new ArrayList<>();
+    // graph isn't necessarily connected
+    for (Entry<Integer, List<Integer>> entry : classes.entrySet()) {
+      Integer current = entry.getKey();
+      if (!parent.containsKey(current)) {
+        parent.put(current, null);
+        dfsVisit(classes, current, parent, order);
+      }
+    }
+    return order;
+  }
+
+  private void dfsVisit(
+    Map<Integer, List<Integer>> classes,
+    Integer current,
+    Map<Integer, Integer> parent,
+    List<Integer> order
+  ) {
+    List<Integer> adj = classes.get(current);
+    for (Integer destination : adj) {
+      if (!parent.containsKey(destination)) {
+        parent.put(destination, current);
+        dfsVisit(classes, destination, parent, order);
+      }
+    }
+    // record dfs post order
+    order.add(current);
+  }
+
 }
