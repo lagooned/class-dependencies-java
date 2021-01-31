d7be13f9352c21ab1f40a1050b3f3ca61f177f8d
Jared Engler <jared.lite@gmail.com>
2021-01-30 23:51:42 -0600

    fail to impl stack dfs

--- src/main/java/com/jaredengler/TopologicalSort.java
+++ src/main/java/com/jaredengler/TopologicalSort.java
@@ -3,48 +3,43 @@ package com.jaredengler;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class TopologicalSort {
 
   public List<Integer> topologicalSort(Map<Integer, List<Integer>> graph) {
-      var list = dfs(graph).stream().collect(Collectors.toList());
-      Collections.reverse(list);
-      return list;
+    var list = dfs(graph).stream().collect(Collectors.toList());
+    //Collections.reverse(list);
+    System.out.println(list);
+    return list;
   }
 
   private List<Integer> dfs(Map<Integer, List<Integer>> graph) {
-    Map<Integer, Integer> parent = new HashMap<>();
-    List<Integer> order = new ArrayList<>();
-    // graph isn't necessarily connected
-    for (Entry<Integer, List<Integer>> entry : graph.entrySet()) {
-      Integer current = entry.getKey();
-      if (!parent.containsKey(current)) {
-        parent.put(current, null);
-        dfsVisit(graph, current, parent, order);
-      }
-    }
-    return order;
-  }
-
-  private void dfsVisit(
-    Map<Integer, List<Integer>> graph,
-    Integer current,
-    Map<Integer, Integer> parent,
-    List<Integer> order
-  ) {
-    List<Integer> adj = graph.get(current);
-    for (Integer destination : adj) {
-      if (!parent.containsKey(destination)) {
-        parent.put(destination, current);
-        dfsVisit(graph, destination, parent, order);
-      }
-    }
-    // record dfs post order
-    order.add(current);
+    Map<Integer, Boolean> visited = new HashMap<>();
+    LinkedList<Integer> order = new LinkedList<>();
+	  Stack<Integer> stack = new Stack<>();
+	  stack.push(0);
+	  
+	  while (!stack.isEmpty()) {
+	    Integer current = stack.pop();
+	  
+	    if (!visited.containsKey(current)) {
+	      order.addFirst(current);
+	      visited.put(current, true);
+	    }
+	  
+	    for (Integer destination : graph.get(current)) {
+	      if (!visited.containsKey(destination)) 
+	        stack.push(destination);
+	    }
+	  }
+	  
+	  return order;
   }
 
 }
