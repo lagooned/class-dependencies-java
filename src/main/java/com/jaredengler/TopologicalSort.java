b1fc02ba4539f716d2cafe0e123e8a7c211cb8f7
Jared Engler <jared.lite@gmail.com>
2021-01-31 01:00:37 -0600

    a small step

--- src/main/java/com/jaredengler/TopologicalSort.java
+++ src/main/java/com/jaredengler/TopologicalSort.java
@@ -14,32 +14,38 @@ public class TopologicalSort {
 
   public List<Integer> topologicalSort(Map<Integer, List<Integer>> graph) {
     var list = dfs(graph).stream().collect(Collectors.toList());
-    //Collections.reverse(list);
+    Collections.reverse(list);
     System.out.println(list);
     return list;
   }
 
   private List<Integer> dfs(Map<Integer, List<Integer>> graph) {
     Map<Integer, Boolean> visited = new HashMap<>();
-    LinkedList<Integer> order = new LinkedList<>();
-	  Stack<Integer> stack = new Stack<>();
-	  stack.push(0);
-	  
-	  while (!stack.isEmpty()) {
-	    Integer current = stack.pop();
-	  
-	    if (!visited.containsKey(current)) {
-	      order.addFirst(current);
-	      visited.put(current, true);
-	    }
-	  
-	    for (Integer destination : graph.get(current)) {
-	      if (!visited.containsKey(destination)) 
-	        stack.push(destination);
-	    }
-	  }
-	  
-	  return order;
+    LinkedList<Integer> postOrder = new LinkedList<>();
+
+    Stack<Integer> stack = new Stack<>();
+    stack.push(0);
+
+    while (!stack.isEmpty()) {
+      Integer current = stack.pop();
+      if (!visited.containsKey(current)) {
+        visited.put(current, true);
+
+        var adj = graph.get(current).stream()
+          .filter(v -> { return !visited.containsKey(v); })
+          .collect(Collectors.toList());
+
+        if (adj.isEmpty()) {
+          postOrder.add(current);
+        }
+
+        for (Integer destination : adj) {
+          stack.push(destination);
+        }
+      }
+    }
+
+    return postOrder;
   }
 
 }
